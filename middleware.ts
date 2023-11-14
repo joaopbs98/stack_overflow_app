d98e4a40b96747aa6a06fff4d12bdbfde0119eae
joaopbs98 <joaopbs98@gmail.com>
2023-11-14 17:01:17 +0000

    Hotfix

--- middleware.ts
+++ middleware.ts
@@ -4,16 +4,16 @@ export default authMiddleware({
   publicRoutes: [
     "/",
     "/api/webhook",
-    "question/id",
+    "question/:id",
     "/tags",
     "/tags/:id",
     "/profile/:id",
     "/community",
     "/jobs",
   ],
-  ignoredRoutes: ["/api/webhook", "api/chatgpt"],
+  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
